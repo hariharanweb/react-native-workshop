ea238b237d270c804b3687a3b45ba1ec98a603b3
HARIHARAN THIAGARAJAN <harihat@thoughtworks.com>
2017-07-18 09:41:49 +0530

    Step1

--- day2/sample-app/src/App.js
+++ day2/sample-app/src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import Customer from './Customer'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-          <h2>Hello world</h2>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Customer />
+        );
+    }
 }
 
 export default App;
---
+++ day2/sample-app/src/Customer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Customer extends Component {
+    render () {
+        return (
+            <div>
+              Welcome Mr.Trump  
+            </div>
+        )
+    }
+}
+
+export default Customer
