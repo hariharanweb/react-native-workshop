7635d19cbcb6bfcd0897673e53ee3fd4f2a42379
HARIHARAN THIAGARAJAN <harihat@thoughtworks.com>
2017-07-20 17:36:47 +0530

    Added movies.js

--- day4/teststore/react-native/TestView.js
+++ day4/teststore/react-native/TestView.js
@@ -1,14 +1,49 @@
 import React, { Component } from 'react'
 import {
     View,
-    Text
+    Text,
+    Button,
+    AsyncStorage,
+    Alert,
+    FlatList
 } from 'react-native'
 
+import Movies from './Movies'
+
 class TestView extends Component {
+
+    constructor(props) {
+        super(props)
+    }
+
+    addData() {
+        // AsyncStorage.setItem('')
+        console.log(Movies)
+        Alert.alert(
+            'Alert Title',
+            'My Alert Msg',
+            [
+                { text: 'Ask me later', onPress: () => console.log('Ask me later pressed') },
+                { text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel' },
+                { text: 'OK', onPress: () => console.log('OK Pressed') },
+            ],
+            { cancelable: true }
+        )
+
+    }
+
+
     render() {
         return (
             <View>
+                <FlatList
+                    data={[{ key: 'a' }, { key: 'b' }]}
+                    renderItem={({ item }) => <Text>{item.key}</Text>}
+                />
                 <Text>Hello</Text>
+                <Button title="Add data"
+                    color="#841584"
+                    onPress={this.addData} />
             </View>
         )
     }
