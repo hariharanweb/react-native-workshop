46a379530465bcd06a0ebc1229efab5972953a57
HARIHARAN THIAGARAJAN <harihat@thoughtworks.com>
2017-07-21 09:36:28 +0530

    Movies

--- day4/teststore/index.ios.js
+++ day4/teststore/index.ios.js
@@ -9,16 +9,49 @@ import {
 	AppRegistry,
 	StyleSheet,
 	Text,
-	View
+	View,
+	FlatList,
+	Alert, 
+	AsyncStorage
 } from 'react-native';
 import TestView from './react-native/TestView'
 
 export default class teststore extends Component {
+
+	constructor(props) {
+		super(props)
+		this.state = {
+			movies: []
+		}
+	}
+
+
+	componentDidMount() {
+		await AsyncStorage.setItem('key', 'value')
+
+		AsyncStorage.getItem('key')
+
+		fetch('https://facebook.github.io/react-native/movies.json')
+			.then((response) =>
+				response.json()
+			).then((responseJson) => {
+				this.setState({
+					movies: responseJson.movies
+				})
+			})
+	}
+
+
 	render() {
+		// Alert.alert('Heading','Some message', [
+		// 	{text: 'Ask me later', onPress: () => console.log('Ask me later pressed')},
+		// ])
 		return (
-			<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-				<TestView />
-			</View>
+			<FlatList
+				style={{ marginTop: 48 }}
+				data={this.state.movies}
+				renderItem={({ item }) => <Text>{item.title}</Text>}
+			/>
 		);
 	}
 }
